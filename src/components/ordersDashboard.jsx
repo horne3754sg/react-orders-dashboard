37dc934570ce02f5284f85f9dabec7d1e78c6280
Matthew Horne <mattjhorne@hotmail.co.uk>
2020-06-06 09:27:16 +0100

    style updates

--- src/components/ordersDashboard.jsx
+++ src/components/ordersDashboard.jsx
@@ -5,7 +5,7 @@ import './OrdersDashboard.scss'
 const orderItems = [...Array(5).keys()].map((_id) => {
   return {
     _id,
-    thumbnail: 'https://picsum.photos/70/70.jpg',
+    thumbnail: '/images/nike-blaze.png',
     brand: 'Nike Air',
     model: `VaporMax 201${_id + 1}`,
     category: 'Men',
@@ -24,27 +24,28 @@ class OrdersDashboard extends Component {
         <div className='results-container'>
           {orderItems.map((order) => (
             <div
+              key={order._id}
               className={`order-item flex-grid status-${order.status.toLowerCase()}`}>
-              <div className='col-900-1-10 thumbnail'>
+              <div className='col-900-2-24 thumbnail'>
                 <img src={order.thumbnail} alt='' />
               </div>
-              <div className='col-900-3-10 product-name'>
+              <div className='col-900-5-24 product-name'>
                 <span className='product-brand'>{order.brand}</span>
                 <span className='product-model'>{order.model}</span>
               </div>
-              <div className='col-900-2-10 product-category'>
-                <span className='cell-label'>Category:</span>{' '}
+              <div className='col-900-5-24 product-category'>
+                <span className='cell-label'>Category:</span>
                 <strong>{order.category}</strong>
               </div>
-              <div className='col-900-2-10 product-size'>
+              <div className='col-900-5-24 product-size'>
                 <span className='cell-label'>Size:</span>
                 <strong>{order.size}</strong>
               </div>
-              <div className='col-900-2-10 product-colour'>
+              <div className='col-900-5-24 product-colour'>
                 <span className='cell-label'>Colour:</span>
                 <strong>{order.colour}</strong>
               </div>
-              <div className='customer-initials'>
+              <div className='col-900-1-24 customer-initials'>
                 <span>{order.customers_initials}</span>
               </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+import './reset.scss'
 import './index.css'
 
 ReactDOM.render(
